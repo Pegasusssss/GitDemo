bd90144ba72034ffe2d8635ea8897946c29833ed
Liam <1642306324@qq.com>
2025-12-03 17:03:38 +0800

    修改

---
+++ src/main/java/org/example/gitdemotest/GitDemoTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.gitdemotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemoTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitDemoTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/gitdemotest/GitDemoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gitdemotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
